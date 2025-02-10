ea4e5ba5ceb108a4cdff4d5d2c4c55adfe2c4246
David Valdman <dmvaldman@gmail.com>
2025-02-10 11:05:05 -0800

    Delete ts folder

--- src/ts/main.ts
+++
@@ -1,9 +0,0 @@
-import './map';
-import { MapManager } from './map';
-
-console.log('Initializing auth...');
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM loaded, initializing MapManager');
-  new MapManager();
-});
