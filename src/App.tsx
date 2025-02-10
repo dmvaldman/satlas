3cf17e2e1de91122c7f4716b158265e76144d19c
David Valdman <dmvaldman@gmail.com>
2025-02-10 10:56:03 -0800

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 import { ProfileProvider } from './contexts/ProfileContext';
 import { MapProvider } from './contexts/MapContext';
