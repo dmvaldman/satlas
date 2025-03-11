096b48594ad663af6a44f261519469d5b36e834f
David Valdman <dmvaldman@gmail.com>
2025-03-11 11:52:53 -0700

    Unify modals

--- src/components/NearbyExistingSitModal.tsx
+++ src/components/NearbyExistingSitModal.tsx
@@ -8,31 +8,56 @@ interface NearbyExistingSitModalProps {
   onUploadToExisting: (sit: Sit) => void;
 }
 
-export default function NearbyExistingSitModal({
-  isOpen,
-  sit,
-  onClose,
-  onUploadToExisting
-}: NearbyExistingSitModalProps) {
-  if (!isOpen || !sit) return null;
+class NearbyExistingSitModal extends React.Component<NearbyExistingSitModalProps> {
+  private modalRef = React.createRef<HTMLDivElement>();
 
-  return (
-    <div className={`modal-overlay${isOpen ? ' active' : ''}`} onClick={onClose}>
-      <div className="photo-options" onClick={(e) => e.stopPropagation()}>
-        <h2>Existing Satlas Nearby</h2>
-        <p>You are close to an existing satlas. Would you like to upload a photo to it?</p>
+  componentDidMount() {
+    this.updateModalAnimation();
+  }
 
-        <button
-          className="photo-option-button"
-          onClick={() => onUploadToExisting(sit)}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/>
-          </svg>
-          Upload Photo
-        </button>
+  componentDidUpdate(prevProps: NearbyExistingSitModalProps) {
+    if (!prevProps.isOpen && this.props.isOpen) {
+      this.updateModalAnimation();
+    } else if (prevProps.isOpen && !this.props.isOpen) {
+      // Modal is closing
+      if (this.modalRef.current) {
+        this.modalRef.current.classList.remove('active');
+      }
+    }
+  }
 
+  private updateModalAnimation() {
+    if (this.modalRef.current) {
+      // Trigger reflow
+      void this.modalRef.current.offsetHeight;
+      this.modalRef.current.classList.add('active');
+    }
+  }
+
+  render() {
+    const { isOpen, sit, onClose, onUploadToExisting } = this.props;
+
+    if (!isOpen || !sit) return null;
+
+    return (
+      <div className="modal-overlay" onClick={onClose}>
+        <div ref={this.modalRef} className="modal-content photo-options" onClick={(e) => e.stopPropagation()}>
+          <h2>Existing Satlas Nearby</h2>
+          <p>You are close to an existing satlas. Would you like to upload a photo to it?</p>
+
+          <button
+            className="photo-option-button"
+            onClick={() => onUploadToExisting(sit)}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/>
+            </svg>
+            Upload Photo
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default NearbyExistingSitModal;
