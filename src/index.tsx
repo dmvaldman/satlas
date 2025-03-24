6079874002a5902d526a861d4c83f1a25790389b
David Valdman <dmvaldman@gmail.com>
2025-03-23 18:17:47 -0700

    Portrait mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { SplashScreen } from '@capacitor/splash-screen';
+import { ScreenOrientation } from '@capacitor/screen-orientation';
 
 // Initialize React app
 const container = document.getElementById('root');
@@ -11,3 +12,6 @@ const root = createRoot(container);
 // Hide the native splash screen once the app is ready
 root.render(<App />);
 SplashScreen.hide();
+
+// Lock the screen orientation to portrait
+ScreenOrientation.lock({ orientation: 'portrait' });
