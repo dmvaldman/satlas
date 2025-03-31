bad6a069fa81308a7e72c2f9273529c884e512ab
David Valdman <dmvaldman@gmail.com>
2025-03-31 12:16:29 -0700

    Remove loading state from Auth button

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -13,26 +13,22 @@ interface AuthProps {
   onUpdatePreferences: (prefs: UserPreferences) => void;
 }
 
-interface AuthState {
-  isSigningIn: boolean;
-}
-
-class AuthComponent extends React.Component<AuthProps, AuthState> {
+class AuthComponent extends React.Component<AuthProps> {
   private defaultProfileImage = './assets/imgs/profile_blank.jpg';
 
   constructor(props: AuthProps) {
     super(props);
-    this.state = {
-      isSigningIn: false
-    };
   }
 
   private handleSignIn = async () => {
-    await this.props.onSignIn();
+    try {
+      await this.props.onSignIn();
+    } catch (error) {
+      console.error('Sign in error:', error);
+    }
   };
 
   private renderLoginButton() {
-    const { isSigningIn } = this.state;
     const isIOS = Capacitor.getPlatform() === 'ios';
 
     return (
@@ -40,18 +36,13 @@ class AuthComponent extends React.Component<AuthProps, AuthState> {
         <button
           className={`auth-button ${isIOS ? 'apple' : 'google'}`}
           onClick={this.handleSignIn}
-          disabled={isSigningIn}
         >
-          {isSigningIn ? (
-            <div className="spinner small"></div>
-          ) : (
-            <>
-              <svg viewBox="0 0 24 24">
-                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/>
-              </svg>
-              {isSigningIn ? 'Signing In...' : 'Sign in'}
-            </>
-          )}
+        <>
+          <svg viewBox="0 0 24 24">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/>
+          </svg>
+          Sign in
+        </>
         </button>
       </div>
     );
@@ -82,12 +73,11 @@ class AuthComponent extends React.Component<AuthProps, AuthState> {
     );
   }
 
-  shouldComponentUpdate(nextProps: AuthProps, nextState: AuthState) {
+  shouldComponentUpdate(nextProps: AuthProps) {
     // Only update if these props/state have changed
     return (
       this.props.user !== nextProps.user ||
-      this.props.isAuthenticated !== nextProps.isAuthenticated ||
-      this.state.isSigningIn !== nextState.isSigningIn
+      this.props.isAuthenticated !== nextProps.isAuthenticated
     );
   }
 
