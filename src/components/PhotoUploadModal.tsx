d23e9cfb0b0691ff70d09d5affbda493ffdbc176
David Valdman <dmvaldman@gmail.com>
2025-04-22 21:59:12 -0700

    Deactivated Take Photo on desktop web

--- src/components/PhotoUploadModal.tsx
+++ src/components/PhotoUploadModal.tsx
@@ -564,21 +564,19 @@ class PhotoUploadComponent extends React.Component<PhotoUploadProps, PhotoUpload
           <div className="offline-notice">You're offline. Photos will upload when you're back online.</div>
         )}
 
-        {/* Only show Take Photo on native platforms OR mobile web */}
-        {(platform !== 'web' || (platform === 'web' && this.isMobileBrowser())) && (
-          <button
-            className="modal-option-button"
-            onClick={this.handleTakePhoto}
-            disabled={processingSource !== null} // Disable if any processing is active
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path d="M12 15.2a3.2 3.2 0 1 0 0-6.4 3.2 3.2 0 0 0 0 6.4z"/>
-              <path d="M9 2L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-3.17L15 2H9zm3 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"/>
-            </svg>
-            Take Photo
-            {processingSource === 'camera' && <div className="spinner xsmall"></div>}
-          </button>
-        )}
+        <button
+          className="modal-option-button"
+          onClick={this.handleTakePhoto}
+          // Disable if processing OR if on desktop web
+          disabled={processingSource !== null || (platform === 'web' && !this.isMobileBrowser())}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M12 15.2a3.2 3.2 0 1 0 0-6.4 3.2 3.2 0 0 0 0 6.4z"/>
+            <path d="M9 2L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-3.17L15 2H9zm3 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"/>
+          </svg>
+          Take Photo
+          {processingSource === 'camera' && <div className="spinner xsmall"></div>}
+        </button>
 
         <button
           className="modal-option-button"
